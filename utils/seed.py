fbc3e07e813bd28cd164ef3906903bdb1260e157
Ioannis Georvasilis <ioannis.georvasilis@gmail.com>
2026-06-10 15:46:03 +0200

    Update seed.py

--- utils/seed.py
+++ utils/seed.py
@@ -1,9 +1,24 @@
-
+import os
 import random
+import numpy as np
 import torch
 
 
 def seed_everything(seed: int) -> None:
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
     random.seed(seed)
+    np.random.seed(seed)
+
     torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+
+    # cuDNN / CUDA determinism
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    # More deterministic PyTorch ops
+    torch.use_deterministic_algorithms(True, warn_only=True)
+
+    return seed
